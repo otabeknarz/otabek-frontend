ef3ca5aa8b2af0830556d7f193cb02e673db9fe2
Otabek Narz <otabeknarz@gmail.com>
2025-11-04 17:57:04 +0500

    implemented posthog

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,20 @@ require("./polyfills/localStorage");
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/ingest/static/:path*",
+        destination: "https://eu-assets.i.posthog.com/static/:path*",
+      },
+      {
+        source: "/ingest/:path*",
+        destination: "https://eu.i.posthog.com/:path*",
+      },
+    ];
+  },
+  // This is required to support PostHog trailing slash API requests
+  skipTrailingSlashRedirect: true,
 };
 
 export default nextConfig;
